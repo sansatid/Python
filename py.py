bcae8be9253eb6eb0d63810b4090cfde92414e96
Chang Sansatid <135413168+sansatid@users.noreply.github.com>
2025-02-14 23:30:06 +0700

    Update py.py

--- py.py
+++ py.py
@@ -1,4 +1,7 @@
-x = int(input("Enter First Number : "))
-y = int(input("Enter Two Number : "))
-z = x * y
-print(z)
+# โปรแกรม Python ง่ายๆ ที่พิมพ์ข้อความออกทางหน้าจอ
+
+def main():
+    print("สวัสดี, โลก!")
+
+if __name__ == "__main__":
+    main()
